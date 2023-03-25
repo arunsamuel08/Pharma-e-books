69d5ff3f9e8018978737775f3c7925c429cf7319
Arun Saji Samuel <arun.samuel08@gmail.com>
2023-03-25 12:51:26 +0530

    MOBILE VIEW DONE

--- decipherdrugs/src/App.js
+++ decipherdrugs/src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCard from "./components/book-card/book-card";
 import Data from "./csvjson.json";
 
 function App() {
+  const [isMobile, setisMobile] = useState(false);
+  useEffect(() => {
+    function handleResize() {
+      setisMobile(window.innerWidth <= 800);
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const [searchTerm, setsearchTerm] = useState("");
   let filteredData = Data;
   if (searchTerm) {
@@ -24,10 +36,36 @@ function App() {
             Need pharmacy books, notes? We gotchu!
           </div>
           <a href="/">
-            <button class="bn54">
-              <span class="bn54span">About Us</span>
+            <button className="bn54">
+              <span className="bn54span">About Us</span>
             </button>
           </a>
+          {isMobile && (
+            <div className="book-container">
+              <div className="search-bar">
+                <input
+                  className="input-box"
+                  type="text"
+                  placeholder="Search..."
+                  onChange={e => setsearchTerm(e.target.value)}
+                ></input>
+                <img
+                  clasname="imgsearch"
+                  src="./images/search.png"
+                  alt="search"
+                />
+              </div>
+              <div id="books" className="books">
+                {filteredData.map(item => (
+                  <BookCard
+                    bookname={item["Name of the Book"]}
+                    link={item["Link"]}
+                    subject={item["Subject"]}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="right-div">
