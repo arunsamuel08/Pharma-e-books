8b69488dc302f1bba05b9ecdd8d8a4237f5fee1c
Arun Saji Samuel <arun.samuel08@gmail.com>
2023-04-19 22:58:52 +0530

    link problems resolved

--- decipherdrugs/src/components/notes/notes.js
+++ decipherdrugs/src/components/notes/notes.js
@@ -5,14 +5,14 @@ import NotesCard from "./notesCard";
 const Notes = () => {
   return (
     <div className="notesContainer">
-      <NotesCard notename="Bpharm 1st Semester Notes" link="www.google.com" />
-      <NotesCard notename="Bpharm 2nd Semester Notes" link="www.google.com" />
-      <NotesCard notename="Bpharm 3rd Semester Notes" link="www.google.com" />
-      <NotesCard notename="Bpharm 4th Semester Notes" link="www.google.com" />
-      <NotesCard notename="Bpharm 5th Semester Notes" link="www.google.com" />
-      <NotesCard notename="Bpharm 6th Semester Notes" link="www.google.com" />
-      <NotesCard notename="Bpharm 7th Semester Notes" link="www.google.com" />
-      <NotesCard notename="Bpharm 8th Semester Notes" link="www.google.com" />
+      <NotesCard notename="Bpharm 1st Semester Notes" link="#" />
+      <NotesCard notename="Bpharm 2nd Semester Notes" link="#" />
+      <NotesCard notename="Bpharm 3rd Semester Notes" link="#" />
+      <NotesCard notename="Bpharm 4th Semester Notes" link="#" />
+      <NotesCard notename="Bpharm 5th Semester Notes" link="#" />
+      <NotesCard notename="Bpharm 6th Semester Notes" link="#" />
+      <NotesCard notename="Bpharm 7th Semester Notes" link="#" />
+      <NotesCard notename="Bpharm 8th Semester Notes" link="#" />
     </div>
   );
 };
